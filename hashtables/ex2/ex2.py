9d651fbdf9f59eb1b9d2b467b963227fdd0935c1
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-03-13 12:29:26 -0400

    working through ex2, still stuck

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,29 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    ## bad answer, quadratic time
+    for ticket in tickets:
+        hash_table_insert(ht, ticket.source, ticket.destination)
 
-    pass
+    for i in range(length):
+        key = hash_table_retrieve(ht, tickets[i].source)
+        if key == "NONE":
+            
+
+
+
+    for i in range(length):
+        hash_table_insert(ht, tickets[i].source, tickets[i].destination)
+
+        # if the ticket has no source, the dest is first in routes
+        if tickets[i].source == "NONE":
+            routes[0] = tickets[i].destination
+        # if ticket has no dest, the source is last in routes
+        elif tickets[i].destination == "NONE":
+            routes[-1] = tickets[i].source
+        
+        else:
+
+    
+    return route
+        
